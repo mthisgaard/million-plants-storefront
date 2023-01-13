a721e3f2f59834de31c1a103b89d1ef9bfe649c9
Malene Thisgaard <malene.thisgaard@gmail.com>
2023-01-13 13:32:47 +0900

    setup cart

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -4,6 +4,7 @@ import {
   useProductOptions,
   ProductPrice,
   BuyNowButton,
+  AddToCartButton,
 } from "@shopify/hydrogen";
 
 export default function ProductDetails({ product }) {
@@ -42,7 +43,6 @@ export default function ProductDetails({ product }) {
 function ProductForm({ product }) {
   const { options, selectedVariant } = useProductOptions();
 
-  const isOutOfStock = !selectedVariant?.availableForSale || false;
   return (
     <form className="grid gap-10">
       {
@@ -81,19 +81,41 @@ function ProductForm({ product }) {
         />
       </div>
       <div className="grid items-stretch gap-4">
-        {isOutOfStock ? (
-          <span className="text-black text-center py-3 px-6 border rounded-sm leading-none ">
-            Available in 2-3 weeks
-          </span>
-        ) : (
-          <BuyNowButton variantId={selectedVariant.id}>
-            <span className="bg-black text-white inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none w-full border">
-              Buy it now
-            </span>
-          </BuyNowButton>
-        )}
+        <PurchaseMarkup />
       </div>
     </form>
+  );
+}
+
+function PurchaseMarkup() {
+  const { selectedVariant } = useProductOptions();
+  const isOutOfStock = !selectedVariant?.availableForSale || false;
+
+  return (
+    <>
+      <AddToCartButton
+        type="button"
+        variantId={selectedVariant.id}
+        quantity={1}
+        accessibleAddingToCartLabel="Adding item to your cart"
+        disabled={isOutOfStock}
+      >
+        <span className="bg-black text-white inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none w-full">
+          {isOutOfStock ? "Sold out" : "Add to cart"}
+        </span>
+      </AddToCartButton>
+      {isOutOfStock ? (
+        <span className="text-black text-center py-3 px-6 border rounded-sm leading-none ">
+          Available in 2-3 weeks
+        </span>
+      ) : (
+        <BuyNowButton variantId={selectedVariant.id}>
+          <span className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none border w-full">
+            Buy it now
+          </span>
+        </BuyNowButton>
+      )}
+    </>
   );
 }
 
